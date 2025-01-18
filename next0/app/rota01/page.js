51bf20b981b3511a920f5ed168bead96f983bdce
Julio-Azevedo <julioazevedodev@gmail.com>
2025-01-17 23:43:57 -0300

    receita next

---
+++ next0/app/rota01/page.js
@@ -0,0 +1,7 @@
+export default function Rota1() {
+    return (
+        <div>
+            <h1>Bem-vindo à Rota 1!</h1>
+        </div>
+    );
+}
---
+++ next0/app/rota02/page.js
@@ -0,0 +1,7 @@
+export default function Rota2() {
+    return (
+        <div>
+            <h1>Bem-vindo à Rota 2!</h1>
+        </div>
+    );
+}
